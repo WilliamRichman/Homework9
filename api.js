d6e0ca2231ebdca295f20e4fc5d9be438e0f2732
William Richman <Goat1120@gmail.com>
2020-02-09 14:31:32 -0700

    Added HTML to page and made new chnages

--- api.js
+++ api.js
@@ -9,5 +9,16 @@ const api = {
             process.exit(1)
         })
     }
+    getStars(userName){
+        return axios.get('https://api.github.com/users/'+ userName +'?client_id=172aff1074ba44d7e995&client_secret=39671eb595bdac737243214cc648b0fc22055c65')
+        .then(response => {
+            console.log(responose.data)
+        })
+        .catch(err => {
+            console.log("User Not Found")
+            process.exit(1)
+        })
+    }
 }
+
 module.exports = api
---
+++ createHtml.js
@@ -0,0 +1,95 @@
+const colors = {
+    blue: {
+        mainBackGround: "blue", 
+        headerBackGround: "green",
+        fontColor: "white",
+        fontColor2: "black"
+    },
+
+    green: {
+        mainBackGround: "green", 
+        headerBackGround: "blue",
+        fontColor: "white",
+        fontColor2: "black"
+    },
+
+    pink: {
+        mainBackGround: "pink", 
+        headerBackGround: "blue",
+        fontColor: "white",
+        fontColor2: "black"
+    },
+
+    red: {
+        mainBackGround: "red", 
+        headerBackGround: "blue",
+        fontColor: "white",
+        fontColor2: "black"
+    }
+};
+
+function createHtml(data){
+    return
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/31d816cc8e.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>GitHub</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header class="header">
+            <img src="#">
+            <h1>HI!</h1>
+            <h2>My Name is ${data.name} </h2>
+            <h4> Currently @ ${data.info}</h4>
+            <nav class="navBar"> 
+                <a href=${data.location} Target="_blank"></a>
+                <a href=${data.github} Target="_blank"></a>
+                <a href=${data.blog} Target="_blank"></a>
+            
+            </nav>
+            </header>
+
+            <main class="main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">I build things and teach people to code! </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <h3>Public respo</h3>
+                            <h5>${data.count}</h5>
+                        </div>
+                        <div class="col">
+                            <h3>Follower</h3>
+                            <h5>${data.count.folow}</h5>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <h3>GitHub Stars</h3>
+                            <h5>${data.count}</h5>
+                        </div>
+                        <div class="col">
+                            <h3>Following</h3>
+                            <h5>${data.count.following}</h5>
+                        </div>
+                </div>
+                </div>
+            </main>
+        
+        </div>
+
+
+        
+    </body>
+    <footer class="foot"></footer>
+    </html>
+    `
+}
